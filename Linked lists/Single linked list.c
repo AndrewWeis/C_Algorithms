ef303bf81f2afa713a94a248e5bd8113cc669136
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-01-22 23:35:33 +0700

    Update Single linked list.c

--- Linked lists/Single linked list.c
+++ Linked lists/Single linked list.c
@@ -1,81 +1,76 @@
-// REWRITE !!! ITS SHIT CODE
-
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
-int i = 0, q = 0, n = 0;
-int first = 0; // указатель на первый эдемент
+struct Node 
+{
+	int data;
+	struct Node* next;
+};
 
-struct node {  // linked list
-	char data[10];
-	int next;
-}arr[1000000];
-
-void add(int pointer) {
-	if (pointer == -1)
-	{
-		arr[n].next = first; // пересваивает значения индекса первого элемента
-		first = n;  // пересваивает сам индекс
-		printf("%d\n", n);
-	}
-	else
-	{
-		arr[n].next = arr[pointer].next;
-		arr[pointer].next = n;
-		printf("%d\n", n);
-	}
+void push(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = (*head_ref);
+    (*head_ref) = new_node;
 }
 
-void del(int pointer) {
-
-	if (pointer == -1)
-	{
-		printf("%s\n", arr[first].data);
-		first = arr[first].next;
-	}
-	else
-	{
-		printf("%s\n", arr[arr[pointer].next].data);
-		arr[pointer].next = arr[arr[pointer].next].next;
-		
-	}
+void insertAfter(struct Node* prev_node, int new_data)
+{
+    if (prev_node == NULL)
+    {
+        printf("the given previous node cannot be NULL");
+        return;
+    }
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->next = prev_node->next;
+    prev_node->next = new_node;
 }
 
-int main() {
-	int sth_do = 0, index_to_do = 0;
-	int tests = 0;
-	freopen("input.txt", "r", stdin);
-	freopen("output.txt", "w", stdout);
-	scanf("%d", &tests);
+void append(struct Node** head_ref, int new_data)
+{
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node));
+    struct Node* last = *head_ref;
+    new_node->data = new_data;
+    new_node->next = NULL;
+ 
+    if (*head_ref == NULL)
+    {
+        *head_ref = new_node;
+        return;
+    }
 
+    while (last->next != NULL)
+        last = last->next;
 
-	for (int f = 0; f < tests; f++) {   // кол-во тестов
-		scanf("%d %d %d", &n, &first, &q);
-		n--;
-		for (int i = 0; i <= n; i++) {
-			scanf("%s %d", &arr[i].data, &arr[i].next); //реализация списка
-		}
+    last->next = new_node;
+}
 
-		for (int j = 1; j <= q; j++) {
-			scanf("%d ", &sth_do);
-			if (sth_do == 1)  // операция удаления
-			{
-				scanf("%d", &index_to_do);
-				del(index_to_do);
-			}
-			else  // операция добавления
-			{
-				n++;
-				scanf("%d %s", &index_to_do, &arr[n].data);
-				add(index_to_do);
-			}
-		}
+void printList(struct Node* n)
+{
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
 
-		printf("===\n");
-		//вывод списка		
-		for (int x = first; x != -1; x = arr[x].next)
-			printf("%s\n", arr[x].data);
-		printf("===\n");
-	}
+int main() 
+{
+    struct Node* head = NULL;
+
+    append(&head, 4);
+    push(&head, 3);
+    append(&head, 5);
+    push(&head, 1);
+    insertAfter(head, 2);
+
+    // head -> 1 -> 2 -> 3 -> 4 -> 5 -> NULL
+
+    printf("Created Linked list is: ");
+    printList(head);
+    printf("\n\n\n\n\n");
+
 	return 0;
-}
+} 
