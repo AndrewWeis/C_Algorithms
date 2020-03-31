39c2c62bc8523330b626567ed8693ffe6c995e41
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-03-31 22:40:09 +0700

    Update Transitive Closure.c

--- Graphs/Transitive Closure.c
+++ Graphs/Transitive Closure.c
@@ -27,9 +27,12 @@ struct growingArr
 };
 
 struct Graph* createGraph(int V);
+void freeGraph(struct Graph* graph, int V, int** tc);
 void addToList(struct Graph* graph, int from, int to);
 void printAdjList(struct Graph* graph, int V);
-void DFSUtil(struct Graph* graph, int** tc, int s, int v)
+
+void DFSUtil(struct Graph* graph, int** tc, int s, int v);
+
 void transitiveClosure(struct Graph* graph, int** tc, int V);
 void createTranClosure(int V, int** tc);
 
@@ -39,7 +42,6 @@ struct Graph* createGraph(int V)
 	graph->V = V;
 	graph->adj = (struct adjList*)malloc(sizeof(struct adjList) * V);
 
-	// inizialize list of adjective vertice to i;  ex 0: 1; 2;
 	for (int i = 0; i < V; i++)
 	{
 		graph->adj[i].grArr = (struct adjList*)malloc(sizeof(struct adjList) * 2);
@@ -51,24 +53,24 @@ struct Graph* createGraph(int V)
 	return graph;
 }
 
-// add vertices into Adjacency list; 
+
 void addToList(struct Graph* graph, int from, int to)
 {
-	int cur_size = graph->adj[from].cur_size; // size of growing array whose index is from
-	int capacity = graph->adj[from].capacity; // max size of growing array whose index is from
+	int cur_size = graph->adj[from].cur_size; 
+	int capacity = graph->adj[from].capacity; 
 
 	if (cur_size == capacity)
 	{
 		struct growingArr* grArr = (struct growingArr*)malloc(sizeof(struct growingArr*) * capacity * 2);
 
-		for (int i = 0; i < cur_size; i++)  // copy all data from old arr to new growing arr
+		for (int i = 0; i < cur_size; i++)  
 			grArr[i] = graph->adj[from].grArr[i];
 
-		grArr[cur_size].vertice = to;  // add new vertice into new growing arr
+		grArr[cur_size].vertice = to;  
 
-		free(graph->adj[from].grArr);  // free memory in old arr
+		free(graph->adj[from].grArr);  
 
-		graph->adj[from].grArr = grArr;  // change pointer to new arr
+		graph->adj[from].grArr = grArr;
 		graph->adj[from].cur_size++;
 		graph->adj[from].capacity *= 2;
 	}
@@ -92,7 +94,6 @@ void printAdjList(struct Graph* graph, int V)
 
 void DFSUtil(struct Graph* graph, int** tc, int s, int v)
 {
-	
 	tc[s][v] = 1;
 
 	for (int j = 0; j < graph->adj[v].cur_size; j++)
@@ -104,7 +105,7 @@ void createTranClosure(int V, int** tc)
 {
 	for (int i = 0; i < V; i++)
 		tc[i] = (int*)malloc(sizeof(int) * V);
-	
+
 	for (int i = 0; i < V; i++)
 		for (int j = 0; j < V; j++)
 			tc[i][j] = 0;
@@ -123,24 +124,36 @@ void transitiveClosure(struct Graph* graph, int** tc, int V)
 	}
 }
 
+void freeGraph(struct Graph* graph, int V, int** tc)
+{
+	for (int i = 0; i < V; i++)
+		free(graph->adj[i].grArr);
+	free(graph->adj);
+	free(graph);
+
+	for (int i = 0; i < V; i++)
+		free(tc[i]);
+	free(tc);
+}
+
 int main()
 {
 	int V = 4;
 	struct Graph* graph = createGraph(V);
+
 	int** tc = (int**)malloc(sizeof(int*) * V);
 	createTranClosure(V, tc);
-	
+
 	addToList(graph, 0, 1);
 	addToList(graph, 0, 2);
 	addToList(graph, 1, 2);
 	addToList(graph, 2, 0);
 	addToList(graph, 2, 3);
 	addToList(graph, 3, 3);
+
 	transitiveClosure(graph, tc, V);
 
-	for (int i = 0; i < V; i++)
-		free(tc[i]);
-	free(tc);
+	freeGraph(graph, V, tc);
 
 	return 0;
 }
