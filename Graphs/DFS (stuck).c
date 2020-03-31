e6037c2163b1183ed3fab420d6cfc2f662adbfaf
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-03-31 22:29:34 +0700

    Update DFS (stuck).c

--- Graphs/DFS (stuck).c
+++ Graphs/DFS (stuck).c
@@ -1,4 +1,4 @@
-// Time complexity O(V+E)
+// Time complexity O(V + E)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,9 +22,12 @@ struct growingArr
 };
 
 struct Graph* createGraph(int V);
+void freeGraph(struct Graph* graph, int V);
 void addToList(struct Graph* graph, int from, int to);
 void printAdjList(struct Graph* graph, int V);
+
 void DFS(struct Graph* graph, int s, int V);
+
 int isFull(struct Stack* stack);
 int isEmpty(struct Stack* stack);
 void push(struct Stack* stack, int item);
@@ -83,7 +86,6 @@ struct Graph* createGraph(int V)
 	graph->V = V;
 	graph->adj = (struct adjList*)malloc(sizeof(struct adjList) * V);
 
-	// inizialize list of adjective vertice to i;  ex 0: 1; 2;
 	for (int i = 0; i < V; i++)
 	{
 		graph->adj[i].grArr = (struct adjList*)malloc(sizeof(struct adjList) * 2);
@@ -96,21 +98,21 @@ struct Graph* createGraph(int V)
 
 void addToList(struct Graph* graph, int from, int to)
 {
-	int cur_size = graph->adj[from].cur_size; // size of growing array whose index is from
-	int capacity = graph->adj[from].capacity; // max size of growing array whose index is from
+	int cur_size = graph->adj[from].cur_size; 
+	int capacity = graph->adj[from].capacity; 
 
 	if (cur_size == capacity)
 	{
 		struct growingArr* grArr = (struct growingArr*)malloc(sizeof(struct growingArr*) * capacity * 2);
 
-		for (int i = 0; i < cur_size; i++)  // copy all data from old arr to new growing arr
+		for (int i = 0; i < cur_size; i++)  
 			grArr[i] = graph->adj[from].grArr[i];
 
-		grArr[cur_size].vertice = to;  // add new vertice into new growing arr
+		grArr[cur_size].vertice = to;  
 
-		free(graph->adj[from].grArr);  // free memory in old arr
+		free(graph->adj[from].grArr);  
 
-		graph->adj[from].grArr = grArr;  // change pointer to new arr
+		graph->adj[from].grArr = grArr; 
 		graph->adj[from].cur_size++;
 		graph->adj[from].capacity *= 2;
 	}
@@ -139,13 +141,13 @@ void DFS(struct Graph* graph, int s, int V)
 	for (int i = 0; i < V; i++)
 		visited[i] = 0;
 
-	
+
 	struct Stack* stack = createStack(V);
 
 	// Push the current source node. 
 	push(stack, s);
 
-	
+
 	int vertice = 0;
 	while (!isEmpty(stack))
 	{
@@ -166,12 +168,24 @@ void DFS(struct Graph* graph, int s, int V)
 		}
 	}
 
+	free(visited);
+	free(stack->array);
+	free(stack);
+}
+
+void freeGraph(struct Graph* graph, int V)
+{
+	for (int i = 0; i < V; i++)
+		free(graph->adj[i].grArr);
+	free(graph->adj);
+	free(graph);
 }
 
 int main()
 {
 	int V = 7;
 	struct Graph* graph = createGraph(V);
+
 	addToList(graph, 1, 2);
 	addToList(graph, 1, 3);
 	addToList(graph, 3, 5);
@@ -180,21 +194,22 @@ int main()
 	addToList(graph, 4, 5);
 	addToList(graph, 5, 6);
 	addToList(graph, 4, 6);
-	printAdjList(graph, V);
+
 	printf("\nDFS: ");
 	DFS(graph, 1, V);
+
+	freeGraph(graph, V);
 
 	return 0;
 }
 
 /*        (1)		Adjective / Incident List
-	    /     \		1: 2; 3;
-	  (2)     (3)	2: 4; 5;
-	   |  \     |	3: 5;
-	   |     \  |	4: 5; 6;
-	  (4) ---- (5)	5: 6;
-		 \    /
-	   (6)
-
-      DFS: 1 3 5 6 2 4
+	/     \		1: 2; 3;
+      (2)     (3)	2: 4; 5;
+       |  \     |	3: 5;
+       |     \  |	4: 5; 6;
+      (4) ---- (5)	5: 6;
+	 \    /
+           (6)
+    DFS: 1 3 5 6 2 4
 */
