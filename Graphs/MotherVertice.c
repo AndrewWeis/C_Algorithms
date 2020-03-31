e70020bc1540b352cdfb7e7fe52e011d25bf0101
AndrewWeis <54636107+AndrewWeis@users.noreply.github.com>
2020-03-31 22:34:00 +0700

    Update MotherVertice.c

--- Graphs/MotherVertice.c
+++ Graphs/MotherVertice.c
@@ -30,10 +30,13 @@ struct growingArr
 };
 
 struct Graph* createGraph(int V);
+void freeGraph(struct Graph* graph, int V);
 void addToList(struct Graph* graph, int from, int to);
 void printAdjList(struct Graph* graph, int V);
+
 void DFS(struct Graph* graph, int s, int V);
 void DFSUtil(struct Graph* graph, int s, int visited[]);
+
 int findMother(struct Graph* graph, int V);
 
 struct Graph* createGraph(int V)
@@ -42,7 +45,6 @@ struct Graph* createGraph(int V)
 	graph->V = V;
 	graph->adj = (struct adjList*)malloc(sizeof(struct adjList) * V);
 
-	// inizialize list of adjective vertice to i;  ex 0: 1; 2;
 	for (int i = 0; i < V; i++)
 	{
 		graph->adj[i].grArr = (struct adjList*)malloc(sizeof(struct adjList) * 2);
@@ -53,24 +55,23 @@ struct Graph* createGraph(int V)
 	return graph;
 }
 
-// add vertices into Adjacency list; 
 void addToList(struct Graph* graph, int from, int to)
 {
-	int cur_size = graph->adj[from].cur_size; // size of growing array whose index is from
-	int capacity = graph->adj[from].capacity; // max size of growing array whose index is from
+	int cur_size = graph->adj[from].cur_size;
+	int capacity = graph->adj[from].capacity;
 
 	if (cur_size == capacity)
 	{
 		struct growingArr* grArr = (struct growingArr*)malloc(sizeof(struct growingArr*) * capacity * 2);
 
-		for (int i = 0; i < cur_size; i++)  // copy all data from old arr to new growing arr
+		for (int i = 0; i < cur_size; i++)  
 			grArr[i] = graph->adj[from].grArr[i];
 
-		grArr[cur_size].vertice = to;  // add new vertice into new growing arr
+		grArr[cur_size].vertice = to;  
 
-		free(graph->adj[from].grArr);  // free memory in old arr
+		free(graph->adj[from].grArr);  
 
-		graph->adj[from].grArr = grArr;  // change pointer to new arr
+		graph->adj[from].grArr = grArr;
 		graph->adj[from].cur_size++;
 		graph->adj[from].capacity *= 2;
 	}
@@ -100,6 +101,8 @@ void DFS(struct Graph* graph, int s, int V)
 		visited[i] = 0;
 
 	DFSUtil(graph, s, visited);
+
+	free(visited);
 }
 
 void DFSUtil(struct Graph* graph, int s, int visited[])
@@ -131,16 +134,16 @@ int findMother(struct Graph* graph, int V)
 			v = i;
 		}
 
-		// If there exist mother vertex(or vetices) in given
-		// graph, then v must be one (or one of them) 
+	// If there exist mother vertex(or vetices) in given
+	// graph, then v must be one (or one of them) 
 
-		// Now check if v is actually a mother vertex (or graph 
-		// has a mother vertex).  We basically check if every vertex 
-		// is reachable from v or not. 
+	// Now check if v is actually a mother vertex (or graph 
+	// has a mother vertex).  We basically check if every vertex 
+	// is reachable from v or not. 
 
-		// Reset all values in visited[] as false and do  
-		// DFS beginning from v to check if all vertices are 
-		// reachable from it or not. 
+	// Reset all values in visited[] as false and do  
+	// DFS beginning from v to check if all vertices are 
+	// reachable from it or not. 
 
 	for (int i = 0; i < V; i++)
 		visited[i] = 0;
@@ -150,8 +153,18 @@ int findMother(struct Graph* graph, int V)
 		if (!visited[i])
 			return -1;
 
+	free(visited);
+
 	return v;
-}		
+}
+
+void freeGraph(struct Graph* graph, int V)
+{
+	for (int i = 0; i < V; i++)
+		free(graph->adj[i].grArr);
+	free(graph->adj);
+	free(graph);
+}
 
 int main()
 {
@@ -166,7 +179,10 @@ int main()
 	addToList(graph, 5, 6);
 	addToList(graph, 5, 2);
 	addToList(graph, 6, 0);
+
 	printf("Mother vertex is %d", findMother(graph, V));
+
+	freeGraph(graph, V);
 
 	return 0;
 }
